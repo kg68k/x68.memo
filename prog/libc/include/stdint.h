c106859b5221e8030e6b18d881bfc1d9557ad2d1
TcbnErik <78926718+kg68k@users.noreply.github.com>
2026-04-07 04:29:27 +0900

    stdint.h: add INT8_*, INT32_*, UINT8_* and UINT32_* macros

--- prog/libc/include/stdint.h
+++ prog/libc/include/stdint.h
@@ -12,15 +12,23 @@ typedef signed char int8_t;
 typedef short int16_t;
 typedef int int32_t;
 
+#define INT8_MIN -128
+#define INT8_MAX 127
 #define INT16_MIN -32768
 #define INT16_MAX 32767
+#define INT32_MIN -2147483648
+#define INT32_MAX 2147483647
 
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 
+#define UINT8_MIN 0
+#define UINT8_MAX 255
 #define UINT16_MIN 0
 #define UINT16_MAX 65535
+#define UINT32_MIN 0
+#define UINT32_MAX 4294967295
 
 #ifdef __cplusplus
 }
